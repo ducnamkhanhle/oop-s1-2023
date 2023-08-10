9a122442effab10d92fb0ba0b63c5d66e8032006
ducnamkhanhle <ducnamkhanh.le@student.adelaide.edu.au>
2023-08-10 15:57:08 +0930

    Assss

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,10 +6,8 @@ using namespace std;
 void print_matrix(int array[10][10]){
     for (ll i = 0; i < 10; i++){
         for (ll j = 0; j < 10; j++){
-            if (j != 10) cout << array[i][j] << " ";
-            else cout << array[i][j];
+            cout << array[i][j] << " ";
         }
-        if (i != 10) cout << endl;
+        cout << endl;
     }
-    
 }
