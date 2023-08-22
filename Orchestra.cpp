e345d29fe506784df52b27c978514ef4a03c4c42
ducnamkhanhle <ducnamkhanh.le@student.adelaide.edu.au>
2023-08-22 17:07:11 +0930

    asdfasfasdf

--- Orchestra.cpp
+++ Orchestra.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 
-Musician::Musician(): instrument("null"), experience(0) {};
+// Musician::Musician(): instrument("null"), experience(0) {};
 
-Musician::Musician(string _instrument, int _experience){
-    instrument = _instrument;
-    experience = _experience;
-}
+// Musician::Musician(string _instrument, int _experience){
+//     instrument = _instrument;
+//     experience = _experience;
+// }
 
-string Musician::get_instrument(){
-    return instrument;
-}
+// string Musician::get_instrument(){
+//     return instrument;
+// }
 
-int Musician::get_experience(){
-    return experience;
-}
+// int Musician::get_experience(){
+//     return experience;
+// }
 
 
 
