91aea3c8966e981c809676208cbe41edf5d75a30
ducnamkhanhle <ducnamkhanh.le@student.adelaide.edu.au>
2023-08-30 13:40:33 +0930

    asdf

--- test1/testing.cpp
+++ test1/testing.cpp
@@ -1,10 +1,44 @@
 #include<iostream>
 #define ll long long
 using namespace std;
- 
+
+
+class Point{
+    private:
+        int* x;
+        int* y;
+    public:
+        Point() {
+            x = new int;
+            y = new int;
+
+            *x = 0;
+            *y = 0;
+        }
+
+        Point(Point& ptr){
+            x = new int;
+            y = new int;
+
+            *x = 0;
+            *y = 0;
+        }
+
+        void print() {cout << *x << " " << *y << endl;}
+        void setA(int _x, int _y) {*x = _x, *y= _y;}
+};
 
 int main(){
-    int **p;
+    Point a;
+    
+    a.setA(6,12);
 
-    cout << p << endl;
+    a.print();
+
+    Point b = a;
+
+    b.setA(10, 10);
+
+    b.print(); 
+    a.print();
 }
